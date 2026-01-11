86007ebf7a00be639955bf2949ac389e9c0f9768
ReddemVarshith <reddemvarshith@gmail.com>
2026-01-11 15:39:53 +0530

    Conf TMDB

--- manacine_project/wsgi.py
+++ manacine_project/wsgi.py
@@ -9,6 +9,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
+# Load .env file
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manacine_project.settings")
